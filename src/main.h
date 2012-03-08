65c435974aba6d82eda93cb96c89835749d6df04
AntoineUbuntu <antoine.suse83@club-internet.fr>
2012-03-08 17:09:56 +0100

    Ajout affichage de la vitesse

--- src/main.h
+++ src/main.h
@@ -4,4 +4,29 @@
 
 #define READ_BUFF_SIZE  42	/*!< Maximum of character */
 
+char *affSpeed[] = {
+    "B0",
+    "B50",
+    "B75",
+    "B110",
+    "B134",
+    "B150",
+    "B200",
+    "B300",
+    "B600",
+    "B1200",
+    "B1800",
+    "B2400",
+    "B4800",
+    "B9600",
+    "B19200",
+    "B38400",
+    "B57600",
+    "B115200",
+    "B230400"
+    };
+
+void read_password(char *buff);
+void afficheSpeed(struct termios *tmpConf);
+
 #endif // MAIN_H_INCLUDED
